c46b45d86d8f91fa2b621f47408b9654960b5c6f
zeyar <zeyarking125@gmail.com>
2020-03-17 12:27:21 +0630

    nav delete

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,7 +7,18 @@
             <div class="content">
                 <!-- <div class="flex-center position-ref full-height"> -->
                     <div class="">
-                        @include('admin/nav')
+                         <div class="links">
+                            <a href="{{ url('admin/home') }}">Home</a>
+                            <a href="{{ url('admin/about') }}">About</a>
+                            <a href="{{ url('admin/html') }}">Html</a>
+                            <a href="{{ url('admin/css') }}">Css</a>
+                            <a href="{{ url('admin/js') }}">Javascript</a>
+                            <a href="{{ url('admin/jquery') }}">JQuery</a>
+                            <a href="{{ url('admin/php') }}">PHP</a>
+                            <a href="{{ url('admin/sql') }}">SQL</a>
+                            <a href="{{ url('admin/c#') }}">C#</a>
+                            <a href="{{ url('admin/ps') }}">Photoshop</a>
+                        </div>
                     </div>
                 <!-- </div> -->
             </div>
